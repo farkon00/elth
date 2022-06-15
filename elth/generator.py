7f5b7c3335e6e0973497c6cd6b4e529c6c4f0532
farkon00 <sammer2016sammer@gmail.com>
2022-06-15 22:28:08 +0300

    Now library can generate basic executables

--- elth/generator.py
+++ elth/generator.py
@@ -2,11 +2,17 @@ from .header import Header
 
 
 class Generator:
-    def __init__(self, header=None):
+    def __init__(self, header=None, program_headers=None):
         if header is None:
             header = Header()
+        if program_headers is None:
+            program_headers = []
 
         self.header = header
+        self.program_headers = program_headers
 
     def generate(self):
-        return self.header.generate()
+        buf = self.header.generate()
+        for i in self.program_headers:
+            buf.extend(i.generate())
+        return buf
--- elth/header.py
+++ elth/header.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class EIdent:
-    osabi: int = 0
+    osabi: int = 3
     
     def generate(self):
         buf = [0x7F, 0x45, 0x4c, 0x46] # ELF magic
@@ -40,12 +40,42 @@ class PType:
     LOPROC = 0x70000000
     HIPRO = 0x7FFFFFFF
 
+class ProgramHeader:
+    def __init__(self,
+     p_type=PType.LOAD, p_flags=0, 
+     p_offset=0, p_vaddr=0, p_paddr=0,
+     p_filesz=0, p_memsz=0, p_align=0):
+        assert p_type < 4294967296, "p_type must fit in 32 bits"
+        assert p_flags < 4294967296, "p_flags must fit in 32 bits"
+        assert p_offset < 18446744073709551615, "p_offset must fit in 64 bits"
+        assert p_vaddr < 18446744073709551615, "p_vaddr must fit in 64 bits"
+        assert p_paddr < 18446744073709551615, "p_paddr must fit in 64 bits"
+        assert p_filesz < 18446744073709551615, "p_filesz must fit in 64 bits"
+        assert p_memsz < 18446744073709551615, "p_memsz must fit in 64 bits"
+        assert p_align < 18446744073709551615, "p_align must fit in 64 bits"
+
+        self.p_type = p_type
+        self.p_flags = p_flags
+        self.p_offset = p_offset
+        self.p_vaddr = p_vaddr
+        self.p_paddr = p_paddr
+        self.p_filesz = p_filesz
+        self.p_memsz = p_memsz
+        self.p_align = p_align
+
+    def generate(self):
+        return [
+            *self.p_type.to_bytes(4, byteorder="little"), *self.p_flags.to_bytes(4, byteorder="little"),
+            *self.p_offset.to_bytes(8, byteorder="little"), *self.p_vaddr.to_bytes(8, byteorder="little"),
+            *self.p_paddr.to_bytes(8, byteorder="little"), *self.p_filesz.to_bytes(8, byteorder="little"),
+            *self.p_memsz.to_bytes(8, byteorder="little"), *self.p_align.to_bytes(8, byteorder="little")
+        ]
 
 class Header:
     def __init__(self, 
      e_ident=None, e_type: int = EType.EXEC, 
-     e_machine: int = 3, e_version: int = 1,
-     e_flags: int = 0):
+     e_machine: int = 0x3e, e_version: int = 1,
+     e_entry: int = 4194424, e_flags: int = 0):
         assert e_type < 65536, "e_type must fit in 16 bits"
         assert e_machine < 65536, "e_machine must fit in 16 bits"
         assert e_version < 4294967296, "e_version must fit in 32 bits"
@@ -58,15 +88,15 @@ class Header:
         self.e_type = e_type
         self.e_machine = e_machine
         self.e_version = e_version
+        self.e_entry = e_entry
         self.e_flags = e_flags
     
     def generate(self):
         buf = self.e_ident.generate()
 
         buf.extend([*self.e_type.to_bytes(2, "little"), *self.e_machine.to_bytes(2, "little"), 
-                    *self.e_version.to_bytes(4, "little")])
+                    *self.e_version.to_bytes(4, "little"), *self.e_entry.to_bytes(8, "little")])
 
-        buf.extend([0] * 8) # e_entry
         buf.append(64) # value of e_phoff
         buf.extend([0] * 7) # padding of e_phoff
         buf.extend([0] * 8) # e_shoff
